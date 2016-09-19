5d49506102ff598b95a54c318a90410c57bf64e9
Jacob Proffer <jacobhardingproffer@gmail.com>
2016-09-18 20:16:05 -0400

    Have logon function return a boolean

--- wopr.rb
+++ wopr.rb
@@ -10,10 +10,12 @@ def list_games()
 end
 
 def logon
+  # authorized_user
+  authorized_user = false
   # user variable
   user = ""
   # while user is not equal to "joshua"
-  while user != "joshua"
+  while user != "joshua" && authorized_user == false
     # prompt the user to log on
     print "Logon: ".upcase
     # store user input in variable user
@@ -22,23 +24,33 @@ def logon
     if user == "joshua"
       # print the string
       puts "Greeting Professor Falken.".upcase
+      # set condition of authorized_user
+      authorized_user = true
       # break out of the loop
       break
     # Otherwise, if user is equal to "help"
     elsif user == "help"
+      # print the string
       puts "No help available.".upcase
     # Otherwise, if user is equal to "help games"
     elsif user == "help games"
+      # call the help_games function
       help_games()
     # Otherwise, if user is equal to "list games"
     elsif user == "list games"
+      # call the list_games function
       list_games()
     # Otherwise
     else
+      # print the string
       puts "Connection terminated".upcase
+      # break out of the loop
       break
     end
   end
+  return authorized_user
 end
 
-logon()
+if logon()
+  puts "Test passes."
+end
