7df104ec6ad505e6308fbe2e32e106b78864afc4
Jacob Proffer <jacobhardingproffer@gmail.com>
2016-09-22 20:15:58 -0400

    Move instance of WOPR to main.rb

--- wopr.rb
+++ wopr.rb
@@ -86,15 +86,3 @@ class WOPR
   end
 
 end
-
-wopr = WOPR.new()
-
-# if logon returns true
-if wopr.logon
-  # call greet_user function
-  wopr.greet_user
-  # pause 1 second
-  sleep 1
-  # call play_game function
-  wopr.play_game
-end
